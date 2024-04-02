eb101d7255ec6ced3fed23d7e4c09ba5e924224e
robemike <robemike1135@gmail.com>
2024-04-02 07:59:43 +0300

    updates index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your code here!
+const mainElement = document.getElementById('main');
+mainElement.parentNode.removeChild(mainElement);
+
+let newHeader = document.createElement('h1');
+newHeader.textContent = "This is the new header";
+
+
+document.body.appendChild(newHeader)
+
+newHeader = document.createElement('h1');
+newHeader.textContent = "This is the new header";
+
+
+newHeader.id = 'victory';
+
+
+document.body.appendChild(newHeader);
+
+newHeader = document.createElement('h1');
+newHeader.textContent = "Mike Robe is the champion";
+
+newHeader.id = 'victory';
+
+document.body.appendChild(newHeader);
